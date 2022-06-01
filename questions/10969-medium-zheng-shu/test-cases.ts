996265585f4658577c63aa4be7a0396e27f96cf0
HuaBing <49869420+hbcraft@users.noreply.github.com>
2022-06-01 08:12:41 +0000

    feat(question): add #10969 - 整数

---
+++ questions/10969-medium-zheng-shu/test-cases.ts
@@ -0,0 +1,13 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+let x = 1
+const Y = 1
+
+type cases = [
+  Expect<Equal<Integer<1>, 1>>,
+  Expect<Equal<Integer<1.1>, never>>,
+  Expect<Equal<Integer<1.0>, 1>>,
+  Expect<Equal<Integer<x>, never>>,
+  Expect<Equal<Integer<Y>, 1>>,
+]
